197846e33263f0549d2018884d75dc9d08cfdd78
James Webster <81452276+JamesWebbDev@users.noreply.github.com>
2023-04-13 15:11:02 +1000

    Doing some c++ tutorial stuff

--- Input_Practice/Source/Input_Practice/Private/TestActor.cpp
+++ Input_Practice/Source/Input_Practice/Private/TestActor.cpp
@@ -9,19 +9,82 @@ ATestActor::ATestActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+
+	Scene->SetupAttachment(GetRootComponent());
+	Mesh->SetupAttachment(Scene);
+
+	Speed		   = 50;
+	Max			   = 200;
+	bIsUp		   = true;
+	bIsInitialised = false;
 }
+
+
 
 // Called when the game starts or when spawned
 void ATestActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	TestImplementableFunc(76);
+
+	const FVector Location = GetActorLocation();
+
+	CurrentZ		= Location.Z;
+	MinHeight		= Location.Z;
+	MaxHeight		= Location.Z + Max;
+	bIsInitialised	= true;
 }
+
+void ATestActor::TestFunc(int32 Val) 
+{
+	UE_LOG(LogTemp, Warning, TEXT("Test Func Called with value %d"), Val);
+}
+
+void ATestActor::TestInputOutput(const int32 InVal, int32& OutVal)
+{
+	OutVal = InVal;
+}
+
+int32 ATestActor::TestPureFunc()const
+{
+	return 25;
+}
+
+void ATestActor::Explode_Implementation()
+{
+	this->Destroy();
+}
+
 
 // Called every frame
 void ATestActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	const float Val = DeltaTime * Speed;
+
+	if (bIsUp) {
+		CurrentZ += Val;
+	}
+	else {
+		CurrentZ -= Val;
+	}
+
+	FVector Location = GetActorLocation();
+	Location.Z = CurrentZ;
+
+	SetActorLocation(Location);
+
+	if (CurrentZ > MaxHeight) {
+		bIsUp = false;
+		Explode();
+	}
+	else if (CurrentZ < MinHeight) {
+		bIsUp = true;
+	}
 
 }
 
--- Input_Practice/Source/Input_Practice/Public/TestActor.h
+++ Input_Practice/Source/Input_Practice/Public/TestActor.h
@@ -16,10 +16,58 @@ public:
 	ATestActor();
 
 protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		USceneComponent* Scene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestActor")
+		uint8 bIsUp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestActor")
+		uint8 bIsInitialised;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestActor")
+		float MinHeight;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestActor")
+		float MaxHeight;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TestActor")
+		float Max;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "TestActor")
+		float CurrentZ;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TestActor")
+		float Speed;
+
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Cone Functions")
+		void TestFunc(int32 Val);
+
+	UFUNCTION(BlueprintCallable, Category = "Cone Functions")
+		void TestInputOutput(const int32 InVal, int32& OutVal);
+
+	UFUNCTION(BlueprintCallable, Category = "Cone Functions")
+		int32 TestPureFunc() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Cone Functions")
+		void TestImplementableFunc(int32 Val);
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Cone Functions")
+		void Explode();
+		void Explode_Implementation();
+
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
