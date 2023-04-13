f52ab96cffdbc82d621c3aec3f7f30df46d4775f
James Webster <81452276+JamesWebbDev@users.noreply.github.com>
2023-04-13 13:32:28 +1000

    Converted project to c++ Project

---
+++ Input_Practice/Source/Input_Practice/Input_Practice.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Input_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Input_Practice, "Input_Practice" );
