9504cf428fa92f35d97ac91bf34eb11247e67b52
James Webster <81452276+JamesWebbDev@users.noreply.github.com>
2023-04-14 14:08:44 +1000

    Added more functionality to GameState

--- Input_Practice/Source/Input_Practice/Private/CPP_TopDownGameState.cpp
+++ Input_Practice/Source/Input_Practice/Private/CPP_TopDownGameState.cpp
@@ -2,13 +2,60 @@
 
 
 #include "CPP_TopDownGameState.h"
+#include "Net/UnrealNetwork.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerState.h"
 
-/*TArray<ACPP_TopDownControllerPlayer*> ACPP_TopDownGameState::GetPlayerControllers()const
+/// <summary>
+/// Replicate Variables in Class.
+/// </summary>
+/// <param name="OutLifetimeProps"></param>
+void ACPP_TopDownGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACPP_TopDownGameState, CharacterList);
+}
+
+/// <summary>
+/// Get Existing 'Top Down Controllers' (Players)
+/// </summary>
+/// <returns></returns>
+TArray<ACPP_TopDownControllerPlayer*>  ACPP_TopDownGameState::GetControllersTopDown() const
+{
+	TArray<ACPP_TopDownControllerPlayer*> ControllerArray;
+
 	for (TObjectPtr<APlayerState> PlayerState : PlayerArray)
 	{
+		APlayerController* ControllerPtr = PlayerState.Get()->GetPlayerController();
+		ACPP_TopDownControllerPlayer* PlayerController = Cast<ACPP_TopDownControllerPlayer>(ControllerPtr);
+		ControllerArray.Add(PlayerController);
+	}
 
+	UE_LOG(LogTemp, Warning, TEXT("Successfully got all TopDown controllers without error!"));
+
+	return ControllerArray;
+}
+
+void ACPP_TopDownGameState::InitialiseCharacters()
+{
+	TArray<AActor*> CharacterArray;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AAICharacter::StaticClass(), CharacterArray);
+
+	for (AActor* Actor : CharacterArray) 
+	{
+		CharacterList.Add(Cast<AAICharacter>(Actor));
 	}
 
 	
-}*/
+}
+
+void ACPP_TopDownGameState::RemoveCharacterFromList(AAICharacter* Character)
+{
+	if (CharacterList.Contains(Character)) 
+	{
+		CharacterList.Remove(Character);
+	}
+}
+
+
+
--- Input_Practice/Source/Input_Practice/Public/CPP_TopDownGameState.h
+++ Input_Practice/Source/Input_Practice/Public/CPP_TopDownGameState.h
@@ -5,17 +5,29 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameState.h"
 #include "CPP_TopDownControllerPlayer.h"
+#include "AICharacter.h"
 #include "CPP_TopDownGameState.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class INPUT_PRACTICE_API ACPP_TopDownGameState : public AGameState
 {
 	GENERATED_BODY()
 	
 public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "CPP", Replicated)
+	TArray<AAICharacter*> CharacterList;
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const;
+
+	UFUNCTION(BlueprintPure, Category = "CPP")
+		TArray<ACPP_TopDownControllerPlayer*> GetControllersTopDown() const;
+
+	UFUNCTION(BlueprintCallable, Category = "CPP")
+		void InitialiseCharacters();
+
+	UFUNCTION(BlueprintCallable, Category = "CPP")
+		void RemoveCharacterFromList(AAICharacter* Character);
 
 
 private:
