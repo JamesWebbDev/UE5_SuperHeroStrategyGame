1269f15a11079c34c7b121c541ebd0aeca0851c7
James Webster <81452276+JamesWebbDev@users.noreply.github.com>
2023-04-20 14:16:35 +1000

    Set up GameMode c++ file

---
+++ Input_Practice/Source/Input_Practice/Public/CPP_TopDownGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CPP_TopDownGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INPUT_PRACTICE_API ACPP_TopDownGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
